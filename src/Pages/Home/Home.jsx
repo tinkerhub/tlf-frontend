daa128a20c1657388d92332086a955c4e753ce97
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-22 11:18:11 +0530

    Main home left side desktop done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,6 +11,39 @@ function Home() {
             <p className="nline-2">Learning Facilitators Program</p>
           </div>
         </div>
+
+        <div className="body-container">
+          <div className="left-side">
+            <p className="main-text">
+              Welcome to
+              <b>
+                TinkerHub’s <br />
+                Learning Facilitator Program
+              </b>
+            </p>
+
+            <p className="sub-text">
+              TinkerHub Learning Facilitators Program 2021 <br />
+              is a{" "}
+              <b>
+                6-month learning initiative by the
+                <br />TinkerHub Foundation {" "}
+              </b>
+              to create a pool of
+              <br /> mentors <b>in 3 different stacks</b> for various
+              <br />
+              learning programs, curating courses,
+              <br />one-to-one mentoring, etc.
+            </p>
+
+            <div className="buttons">
+              <button className="signup">Sign Up</button>
+              <button className="login">Login</button>
+            </div>
+          </div>
+
+          <div className="right-side"></div>
+        </div>
       </div>
     </div>
   );
