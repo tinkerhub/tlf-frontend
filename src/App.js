3d3fa2e26250ba9cdee7e938793c6e705e75af56
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-22 10:36:50 +0530

    Navbar done for main home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>TinkerHub Learning Facilitator's Website</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <div className="main-container">
+        <div className="navbar">
+          <div className="nav-text">
+            <p className="nline-1">TinkerHub</p>
+            <p className="nline-2">Learning Facilitators Program</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
