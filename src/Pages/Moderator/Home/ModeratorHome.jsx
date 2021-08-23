8be60b3ccdacbf1ceefd1c6d1bec7ae52e1d4b22
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-23 16:21:01 +0530

    Stack Filter added for moderator home and is responsive

--- src/Pages/Moderator/Home/ModeratorHome.jsx
+++ src/Pages/Moderator/Home/ModeratorHome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HomeNavBar from "../../../Components/HomeNavBar";
+import HomeSideHeader from "../../../Components/HomeSideHeader";
 import Welcome from "../../../Components/Welcome";
 
 import "./ModeratorHome.css";
@@ -10,6 +11,17 @@ function ModeratorHome() {
       <div className="momain-container">
         <HomeNavBar />
         <Welcome name="Abhinav K" role="moderator" stack="" />
+        <div className="orange-bars">
+          <HomeSideHeader label="TLF Mentors" />
+
+          <div className="filter">
+            <div className="row1">
+              <p>Flutter</p>
+              <p>JavaScript</p>
+            </div>
+            <div className="row2">Python</div>
+          </div>
+        </div>
       </div>
     </div>
   );
