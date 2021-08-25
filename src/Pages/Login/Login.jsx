6ec33a34062a65825098c892a00008ad8f021d6f
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-25 16:46:23 +0530

    Login Page Done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,24 +1,41 @@
 import PurpleBox from "../../Components/PurpleBox";
 import LoginTextField from "../../Components/LoginTextField";
-import OrangeButton from "../../Components/OrangeButton";
 
 import "./Login.css";
 
 function LogInPage() {
-    return (
-        <div className="lcontainer">
-            <PurpleBox>
-                <div className="login-form">
-                    <div style={{ height: "68px" }} />
-                    <LoginTextField label="email or username" />
-                    <LoginTextField label="password" />
-                    <div style={{ height: "200px" }} />
-                    <p className="help-text">Don't have an account? <p className="help-text-bold">Register</p> </p>
-                    <OrangeButton label="Sign In" style={{ width: "97%", margin: 0, fontSize: "1.4rem" }} />
-                </div>
-            </PurpleBox>
+  return (
+    <div className="lcontainer">
+      <div className="lleft-side">
+        <div className="lcontents">
+          <p className="lheader">
+            Welcome to <b>TinkerHub Learning Facilitator Program.</b>
+          </p>
+          <p className="lsubs">
+            TinkerHub Learning Facilitators Program 2021 is a{" "}
+            <b>6-month learning initiative by the TinkerHub Foundation</b> to
+            create a pool of mentors in<b> 3 different stacks</b> for various
+            learning programs, curating courses, one-to-one mentoring, etc.
+          </p>
         </div>
-    );
+      </div>
+      <div className="lright-side">
+        <PurpleBox>
+          <div className="login-form">
+            <LoginTextField label="email or username" />
+            <LoginTextField label="password" />
+          </div>
+
+          <div className="login-buttons">
+            <p className="login-text">
+              Don't have an account? <b>Create Account</b>
+            </p>
+            <button className="lbtn">Login </button>
+          </div>
+        </PurpleBox>
+      </div>
+    </div>
+  );
 }
 
 export default LogInPage;
