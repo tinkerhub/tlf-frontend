99ea9efbd1ef1404f68e5867249498f70d41ef9c
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-25 16:56:44 +0530

    except for some minor issues signup page also done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -5,22 +5,39 @@ import OrangeButton from "../../Components/OrangeButton";
 import "../Login/Login.css";
 
 function SignUpPage() {
-    return (
-        <div className="lcontainer">
-            <PurpleBox>
-                <div className="login-form">
-                    <div style={{ height: "28px" }} />
-                    <LoginTextField label="Full Name" />
-                    <LoginTextField label="email or username" />
-                    <LoginTextField label="password" />
-                    <LoginTextField label="Flutter/Python/Javascript" />
-                    <div style={{ height: "60px" }} />
-                    <p className="help-text">Already have an account? <p className="help-text-bold">Login</p> </p>
-                    <OrangeButton label="Create Account" style={{ width: "97%", margin: 0, fontSize: "1.4rem" }} />
-                </div>
-            </PurpleBox>
+  return (
+    <div className="lcontainer">
+      <div className="lleft-side">
+        <div className="lcontents">
+          <p className="lheader">
+            Welcome to <b>TinkerHub Learning Facilitator Program.</b>
+          </p>
+          <p className="lsubs">
+            TinkerHub Learning Facilitators Program 2021 is a{" "}
+            <b>6-month learning initiative by the TinkerHub Foundation</b> to
+            create a pool of mentors in<b> 3 different stacks</b> for various
+            learning programs, curating courses, one-to-one mentoring, etc.
+          </p>
         </div>
-    );
+      </div>
+      <div className="lright-side">
+        <PurpleBox>
+          <div className="login-form">
+            <LoginTextField label="Full Name" />
+            <LoginTextField label="email or username" />
+            <LoginTextField label="password" />
+            <LoginTextField label="Flutter/Python/Javascript" />
+          </div>
+          <div className="login-buttons">
+            <p className="login-text">
+              Already have an account? <b>Login</b>
+            </p>
+            <button className="lbtn">Create Account</button>
+          </div>
+        </PurpleBox>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpPage;
