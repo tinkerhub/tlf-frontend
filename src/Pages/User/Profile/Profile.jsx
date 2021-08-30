3c54b9669e464f1bd2dd425934228a6945fb3615
Aswin Asok <aswinasokofficial@gmail.com>
2021-08-30 11:27:50 +0530

    Profile Page Done

--- src/Pages/User/Profile/Profile.jsx
+++ src/Pages/User/Profile/Profile.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import HomeNavBar from "../../../Components/HomeNavBar";
 import HomeSideHeader from "../../../Components/HomeSideHeader";
 
+import JavaScriptProfile from "./JavaScriptProfile.png";
+import "./Profile.css";
+
 function Profile() {
   return (
     <div>
@@ -15,7 +18,33 @@ function Profile() {
             </div>
           </div>
           <div className="profile-main">
-              
+            <div className="profile-pic">
+              <img src={JavaScriptProfile} alt="" className="profile-image" />
+            </div>
+            <div className="profile-text">
+              <p className="profile-name">
+                <b>Lorem Ipsum</b>
+              </p>
+              <p className="profile-email">loremipsum@tinkerhub.com</p>
+              <button className="profile-pass">Change Password?</button>
+            </div>
+
+            <div className="profile-table">
+              <p className="table-header">Unfinished Tasks</p>
+              <table id="pro-task">
+                <tr>
+                  <th>Name</th>
+                  <th>Assigned Date</th>
+                  <th>Due Date</th>
+                </tr>
+
+                <tr>
+                  <td>Lorem Ipsum</td>
+                  <td>23-08-21</td>
+                  <td>23-08-21</td>
+                </tr>
+              </table>
+            </div>
           </div>
         </div>
       </div>
