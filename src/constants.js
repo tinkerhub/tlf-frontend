0c642e783a015a9542279160e00c3237acbb1290
Aswin Asok <aswinasokofficial@gmail.com>
2021-09-04 16:42:16 +0530

    Removed Unwanted File

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-// don't put trailing '/'
-export const serverUrl = 'http://localhost:8000';
-
-// export const jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoZWxsb0BoZWxsby5jb20iLCJleHAiOjE2MzA1MDUwMDd9.Xwb6ozcqLLqKktFvQa2FqkfO0YdfLCpAJ-y1uI09ICk";
-export const jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoZWhlQGhlaGUuY29tIiwiZXhwIjoxNjMwNTE1Nzg5fQ.O9-tyb-zwweQH69w5IObSS1AEnIj2XomRO_f0z0t29A";
