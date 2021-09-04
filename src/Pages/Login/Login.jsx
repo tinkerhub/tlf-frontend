2f955a4dbcc73548908d7d03514ef583097503fb
Aswin Asok <aswinasokofficial@gmail.com>
2021-09-04 13:04:37 +0530

    Snackbar added in login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,82 +7,83 @@ import LoginTextField from "../../Components/LoginTextField";
 import "./Login.css";
 import { login } from "./loginapi";
 import { getProfile } from "../User/Home/userhomeapi";
+import CustomizedSnackbars from "../../Components/AlertSnackBar";
 
 function LogInPage({ setTokenReceived, profile }) {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [error, setError] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     let res = await login(email, password, setTokenReceived);
     if (res === "error") {
-      alert("Login failed");
-      window.location.reload();
-    }
-
-    else if (res === "success") {
+      setError("Login Failed");
+      setTimeout(function(){ window.location.reload(); }, 4000);
+    } else if (res === "success") {
       setTokenReceived(true);
       profile = await getProfile();
 
       if (!profile) {
-      }
-      else if (profile === "error") {
+      } else if (profile === "error") {
         alert("Login failed");
-      }
-      else if (profile.role === "ADMIN") {
-
+      } else if (profile.role === "ADMIN") {
         window.location.replace("/admin/home");
-
       } else if (profile.role === "MODERATOR") {
         window.location.replace("/moderator/home");
-
       } else if (profile.role === "USER") {
         window.location.replace("/user/home");
       }
     }
-  }
-
+  };
 
   return (
-    <div className="lcontainer">
-      <div className="lleft-side">
-        <div className="lcontents">
-          <p className="lheader">
-            Welcome to <b>TinkerHub Learning Facilitator Program.</b>
-          </p>
-          <p className="lsubs">
-            TinkerHub Learning Facilitators Program 2021 is a{" "}
-            <b>6-month learning initiative by the TinkerHub Foundation</b> to
-            create a pool of mentors in<b> 3 different stacks</b> for various
-            learning programs, curating courses, one-to-one mentoring, etc.
-          </p>
+    <div className="mainanderror">
+      {(() => {
+        if (error) {
+          return <CustomizedSnackbars error={error} />;
+        }
+      })()}
+      <div className="lcontainer">
+        <div className="lleft-side">
+          <div className="lcontents">
+            <p className="lheader">
+              Welcome to <b>TinkerHub Learning Facilitator Program.</b>
+            </p>
+            <p className="lsubs">
+              TinkerHub Learning Facilitators Program 2021 is a{" "}
+              <b>6-month learning initiative by the TinkerHub Foundation</b> to
+              create a pool of mentors in<b> 3 different stacks</b> for various
+              learning programs, curating courses, one-to-one mentoring, etc.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="lright-side">
-        <form onSubmit={handleSubmit} >
-          <PurpleBox>
-            <div className="login-form">
-              <LoginTextField label="email or username" setValue={setEmail} />
-              <LoginTextField label="password" type="password" setValue={setPassword} />
-            </div>
+        <div className="lright-side">
+          <form onSubmit={handleSubmit}>
+            <PurpleBox>
+              <div className="login-form">
+                <LoginTextField label="email or username" setValue={setEmail} />
+                <LoginTextField
+                  label="password"
+                  type="password"
+                  setValue={setPassword}
+                />
+              </div>
 
-            <div className="login-buttons">
-              <Link to="/signup">
-                <p className="login-text">
-                  Don't have an account? <b>Create Account</b>
-                </p>
-              </Link>
+              <div className="login-buttons">
+                <Link to="/signup">
+                  <p className="login-text">
+                    Don't have an account? <b>Create Account</b>
+                  </p>
+                </Link>
 
-              <button
-                className="lbtn"
-              >
-                Login{" "}
-              </button>
+                <button className="lbtn">Login </button>
 
-              <p className="forgot-text">Forgot Password ?</p>
-            </div>
-          </PurpleBox>
-        </form>
+                <p className="forgot-text">Forgot Password ?</p>
+              </div>
+            </PurpleBox>
+          </form>
+        </div>
       </div>
     </div>
   );
